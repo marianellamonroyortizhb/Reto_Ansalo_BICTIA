89deb84bcf57b8a30ac31cc6dff2f89597ba76ce
marianellamonroyortizhb <1880@holbertonschool.com>
2020-09-25 23:57:37 -0500

    Actualizar Reto Ansalo

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+//Operaciones matemáticas
+
+function suma(num1, num2){
+	let result_suma = num1 + num2
+	return result_suma
+}
+total.innerHTML = resul_suma;
+
+function resta(num1, num2){
+	let result_resta = num1 - num2
+	return result_resta
+}
+total.innerText = resul_resta;
+
+function multip(num1, num2){
+	let result_multip = num1 * num2
+	return result_multip
+}
+total.innerText = resul_mult;
+
+function division(num1, num2){
+	let result_div = num1 / num2
+	return result_div
+}
+total.innerText = resul_div;
